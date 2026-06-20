ff63d0fd966067c22f49152c26cb996479101c48
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-06-20 14:57:02 +0300

    about_page_mobile => _

--- apps/Client/src/pages/about_page/sections/about_section_four/about-section-four.ts
+++ apps/Client/src/pages/about_page/sections/about_section_four/about-section-four.ts
@@ -1,10 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AboutSectionFourData } from './ui_fct';
+import { ImgLoading } from '@/common/components/general/img_loading/img-loading';
 
 @Component({
   selector: 'app-about-section-four',
-  imports: [],
+  imports: [ImgLoading],
   templateUrl: './about-section-four.html',
   styleUrl: './about-section-four.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AboutSectionFour {}
+export class AboutSectionFour {
+  public readonly AboutSectionFourData = AboutSectionFourData;
+}
