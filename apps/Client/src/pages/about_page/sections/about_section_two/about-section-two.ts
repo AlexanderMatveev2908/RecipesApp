3533ada94b4826060d30930e085674e4ac02bfac
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-06-20 08:05:53 +0300

    about_page_mobile => _

--- apps/Client/src/pages/about_page/sections/about_section_two/about-section-two.ts
+++ apps/Client/src/pages/about_page/sections/about_section_two/about-section-two.ts
@@ -1,10 +1,18 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AboutSectionTwoData } from './ui_fct';
+import { SvgT } from '@/common/types/general';
+import { SvgAdvArrowRight } from '@/common/components/svgs/advanced/arrow_right/arrow-right';
+import { NgComponentOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-about-section-two',
-  imports: [],
+  imports: [NgComponentOutlet],
   templateUrl: './about-section-two.html',
   styleUrl: './about-section-two.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AboutSectionTwo {}
+export class AboutSectionTwo {
+  public readonly AboutSectionTwoData = AboutSectionTwoData;
+
+  public readonly SvgRight: SvgT = SvgAdvArrowRight;
+}
